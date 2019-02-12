a642b86d6eea6956b745038b29904abc4b512a41
Joel Bennett <Joel.Bennett@Questionmark.com>
2019-02-11 19:08:36 -0500

    First Draft

---
+++ ErrorView.psd1
@@ -0,0 +1,43 @@
+@{
+    Description          = 'Enhances formatting ability for Errors'
+    GUID                 = '5857f85c-8a0a-44e1-8da5-c8ff352167e0'
+    Author               = 'Joel Bennett'
+    CompanyName          = 'PoshCode'
+
+    RootModule           = 'ErrorView.psm1'
+    FormatsToProcess     = 'ErrorView.ps1xml'
+    ModuleVersion        = '0.0.1'
+    CompatiblePSEditions = @("Core", "Desktop")
+
+    Copyright            = '(c) Joel Bennett. All rights reserved.'
+
+    FunctionsToExport    = 'Write-NativeCommandError', 'ConvertTo-ErrorCategoryView', 'ConvertTo-ErrorNormalView', 'ConvertTo-ErrorSimpleView'
+
+    CmdletsToExport = @()
+    VariablesToExport = @()
+    AliasesToExport = @()
+
+PrivateData = @{
+    PSData = @{
+        # Tags applied to this module. These help with module discovery in online galleries.
+        # Tags = @()
+
+        # A URL to the license for this module.
+        # LicenseUri = ''
+
+        # A URL to the main website for this project.
+        # ProjectUri = ''
+
+        # A URL to an icon representing this module.
+        # IconUri = ''
+
+        # ReleaseNotes of this module
+        # ReleaseNotes = ''
+
+    } # End of PSData hashtable
+
+} # End of PrivateData hashtable
+
+# HelpInfoURI = ''
+}
+
---
+++ ErrorView.psm1
@@ -0,0 +1,165 @@
+function Write-NativeCommandError {
+    [CmdletBinding()]
+    param(
+        [System.Management.Automation.ErrorRecord]
+        $CurrentError
+    )
+
+    if ($CurrentError.FullyQualifiedErrorId -eq "NativeCommandErrorMessage") { return }
+
+    $myinv = $CurrentError.InvocationInfo
+    if ($myinv -and $myinv.MyCommand) {
+        switch -regex ( $myinv.MyCommand.CommandType ) {
+            ([System.Management.Automation.CommandTypes]::ExternalScript) {
+                if ($myinv.MyCommand.Path) {
+                    $myinv.MyCommand.Path + " : "
+                }
+                break
+            }
+            ([System.Management.Automation.CommandTypes]::Script) {
+                if ($myinv.MyCommand.ScriptBlock) {
+                    $myinv.MyCommand.ScriptBlock.ToString() + " : "
+                }
+                break
+            }
+            default {
+                if ($myinv.InvocationName -match '^[&amp;\.]?$') {
+                    if ($myinv.MyCommand.Name) {
+                        $myinv.MyCommand.Name + " : "
+                    }
+                } else {
+                    $myinv.InvocationName + " : "
+                }
+                break
+            }
+        }
+    } elseif ($myinv -and $myinv.InvocationName) {
+        $myinv.InvocationName + " : "
+    }
+}
+
+function ConvertTo-ErrorCategoryView {
+    [CmdletBinding()]
+    param(
+        [System.Management.Automation.ErrorRecord]
+        $CurrentError
+    )
+
+    $CurrentError.CategoryInfo.GetMessage()
+}
+
+
+function ConvertTo-ErrorSimpleView {
+    [CmdletBinding()]
+    param(
+        [System.Management.Automation.ErrorRecord]
+        $CurrentError
+    )
+
+    if ($CurrentError.FullyQualifiedErrorId -eq "NativeCommandErrorMessage") {
+        $CurrentError.Exception.Message
+    } else {
+        $myinv = $CurrentError.InvocationInfo
+        if ($myinv -and ($myinv.MyCommand -or ($CurrentError.CategoryInfo.Category -ne 'ParserError'))) {
+            $posmsg = $myinv.PositionMessage
+        } else {
+            $posmsg = ""
+        }
+
+        if ($posmsg -ne "") {
+            $posmsg = "`n" + $posmsg
+        }
+
+        if ( & { Set-StrictMode -Version 1; $CurrentError.PSMessageDetails } ) {
+            $posmsg = " : " + $CurrentError.PSMessageDetails + $posmsg
+        }
+
+        $indent = 4
+        $width = $host.UI.RawUI.BufferSize.Width - $indent - 2
+
+        $originInfo = & { Set-StrictMode -Version 1; $CurrentError.OriginInfo }
+        if (($null -ne $originInfo) -and ($null -ne $originInfo.PSComputerName)) {
+            $indentString = "+ PSComputerName        : " + $originInfo.PSComputerName
+            $posmsg += "`n"
+            foreach ($line in @($indentString -split "(.{$width})")) {
+                if ($line) {
+                    $posmsg += (" " * $indent + $line)
+                }
+            }
+        }
+
+        if (!$CurrentError.ErrorDetails -or !$CurrentError.ErrorDetails.Message) {
+            $CurrentError.Exception.Message + $posmsg + "`n "
+        } else {
+            $CurrentError.ErrorDetails.Message + $posmsg
+        }
+    }
+}
+
+function ConvertTo-ErrorNormalView {
+    [CmdletBinding()]
+    param(
+        [System.Management.Automation.ErrorRecord]
+        $CurrentError
+    )
+
+    if ($CurrentError.FullyQualifiedErrorId -eq "NativeCommandErrorMessage") {
+        $CurrentError.Exception.Message
+    } else {
+        $myinv = $CurrentError.InvocationInfo
+        if ($myinv -and ($myinv.MyCommand -or ($CurrentError.CategoryInfo.Category -ne 'ParserError'))) {
+            $posmsg = $myinv.PositionMessage
+        } else {
+            $posmsg = ""
+        }
+
+        if ($posmsg -ne "") {
+            $posmsg = "`n" + $posmsg
+        }
+
+        if ( &{ Set-StrictMode -Version 1; $CurrentError.PSMessageDetails } ) {
+            $posmsg = " : " +  $CurrentError.PSMessageDetails + $posmsg
+        }
+
+        $indent = 4
+        $width = $host.UI.RawUI.BufferSize.Width - $indent - 2
+
+        $errorCategoryMsg = &{ Set-StrictMode -Version 1; $CurrentError.ErrorCategory_Message }
+        if ($null -ne $errorCategoryMsg) {
+            $indentString = "+ CategoryInfo            : " + $CurrentError.ErrorCategory_Message
+        } else {
+            $indentString = "+ CategoryInfo            : " + $CurrentError.CategoryInfo
+        }
+        $posmsg += "`n"
+        foreach ($line in @($indentString -split "(.{$width})")) {
+            if ($line) {
+                $posmsg += (" " * $indent + $line)
+            }
+        }
+
+        $indentString = "+ FullyQualifiedErrorId   : " + $CurrentError.FullyQualifiedErrorId
+        $posmsg += "`n"
+        foreach ($line in @($indentString -split "(.{$width})")) {
+            if ($line) {
+                $posmsg += (" " * $indent + $line)
+            }
+        }
+
+        $originInfo = &{ Set-StrictMode -Version 1; $CurrentError.OriginInfo }
+        if (($null -ne $originInfo) -and ($null -ne $originInfo.PSComputerName)) {
+            $indentString = "+ PSComputerName          : " + $originInfo.PSComputerName
+            $posmsg += "`n"
+            foreach ($line in @($indentString -split "(.{$width})")) {
+                if ($line) {
+                    $posmsg += (" " * $indent + $line)
+                }
+            }
+        }
+
+        if (!$CurrentError.ErrorDetails -or !$CurrentError.ErrorDetails.Message) {
+            $CurrentError.Exception.Message + $posmsg + "`n "
+        } else {
+            $CurrentError.ErrorDetails.Message + $posmsg
+        }
+    }
+}
